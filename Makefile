e3de94b68e88faee65d87fcf6db049a84495b723
Simeon McGraw <simeon@smcgraw.com>
2019-11-23 17:57:36 +0000

    Added output redirection support

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 CC=gcc
-CFLAGS=
+CFLAGS=-Wall
 
 make: src/grsh.c
 	$(CC) -o build/grsh src/grsh.c
+makedebug: src/grsh.c
+	$(CC) -g -o build/grsh src/grsh.c
 run: make
 	build/grsh
+debug: makedebug
+	gdb build/grsh
--- src/grsh.c
+++ src/grsh.c
@@ -8,7 +8,7 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
-#define CMD_DEL " \t\n"
+#define WHITESPACE " \t\n\v\f\r"
 
 typedef struct pathnode {
 	char *path;
@@ -21,6 +21,16 @@ void deletePath(pathnode_t *n) {
 		free(n->path);
 		free(n);
 		n = next;
+	}
+}
+
+void trim_whitespace(char **str) {
+	// trim spaces
+	while (isspace((*str)[0])) {
+		(*str) ++;
+	}
+	for (int i=strlen(*str)-1; i >= 0 && isspace((*str)[i]); i--) {
+		(*str)[i] = 0;
 	}
 }
 
@@ -57,6 +67,9 @@ char * get_full_path(pathnode_t *path, char *cmd) {
 }
 
 int count_words (char *str) {
+	if (str == NULL) {
+		return 0;
+	}
 	int numWords = 0;
 	bool isWord = false;
 	while(*str != 0) {
@@ -77,23 +90,28 @@ char ** split_args (char *cmd, char *argstr) {
 	
 	char *saveptr = NULL;
 	args[0] = cmd;
+	args[len] = NULL;
+	if (argstr == NULL) {
+		return args;
+	}
 	int idx = 1;
-	char * arg = strtok_r(argstr, CMD_DEL, &saveptr);
+	char * arg = strtok_r(argstr, WHITESPACE, &saveptr);
 	while (arg != NULL && idx <= len) {
 		args[idx++] = arg;
-		arg = strtok_r(NULL, CMD_DEL, &saveptr);
+		arg = strtok_r(NULL, WHITESPACE, &saveptr);
 	}
-	args[len] = NULL;
 	return args;
 }
 
 void execute_cmd (char *full_cmd, char **argv, char *output, bool async) {
+	char *out = strdup(output);
 	int id = fork();
 	if (id == 0) { // child
-		if (output != NULL) {
+		if (out != NULL && strlen(out) > 0) {
 			close(STDOUT_FILENO);
 			close(STDERR_FILENO);
-			open(output, O_WRONLY);
+			open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+			open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666);
 		}
 		execv(full_cmd, argv);
 	} else {
@@ -108,15 +126,19 @@ void process_cmd (pathnode_t **path, char *cmd, char *argstr, bool async) {
 		exit(0);
 		return;
 	} else if (strcmp(cmd, "cd") == 0) {
+		if (argstr != NULL) {
+			trim_whitespace(&argstr);
+		}
 		if (chdir(argstr) == -1) {
-			printf("cd: command failed.");
+			char error_message[30] = "An error has occurred\n";
+			write(STDERR_FILENO, error_message, strlen(error_message));
 		}
 	} else if (strcmp(cmd, "path") == 0) {
 		deletePath(*path);
 		(*path) = NULL;
 		pathnode_t * curr_node = NULL;
 		char *saveptr = NULL;
-		char *arg = strtok_r(argstr, CMD_DEL, &saveptr);
+		char *arg = strtok_r(argstr, WHITESPACE, &saveptr);
 		while (arg != NULL) {
 			pathnode_t * node = addPath(arg);
 			if (curr_node == NULL) {
@@ -126,7 +148,7 @@ void process_cmd (pathnode_t **path, char *cmd, char *argstr, bool async) {
 				curr_node->next = node;
 				curr_node = curr_node->next;
 			}
-			arg = strtok_r(NULL, CMD_DEL, &saveptr);
+			arg = strtok_r(NULL, WHITESPACE, &saveptr);
 		}
 	} else if(strlen(cmd) > 0) {
 		char *full_path = get_full_path(*path, cmd);
@@ -137,21 +159,24 @@ void process_cmd (pathnode_t **path, char *cmd, char *argstr, bool async) {
 			return;
 		}
 
-		/*
-		char *saveptr = NULL;
-		char *args = strtok_r(argstr, ">", &saveptr);
-		char *output_file = strtok_r(NULL, ">", &saveptr);
-		// trim spaces
-		while (output_file[0] == ' ') {
-			output_file ++;
+		char *rest = NULL;
+		char *args = NULL;
+		char *output_file = NULL;
+		while (isspace(argstr[0])) {
+			argstr++;
 		}
-		for (int i=strlen(output_file)-1; i >= 0 && output_file[i] == ' '; i--) {
-			output_file[i] = 0;
+		if (argstr[0] == '>') {
+			output_file = argstr + 1;
+		} else {
+			args = strtok_r(argstr, ">", &rest);
+			output_file = rest;
 		}
-		*/
+		if (output_file != NULL) {
+			trim_whitespace(&output_file);
+		}
 
-		char **argv = split_args(full_path, argstr);
-		execute_cmd(full_path, argv, NULL, async);
+		char **argv = split_args(cmd, args);
+		execute_cmd(full_path, argv, output_file, async);
 		free(argv);
 	}
 }
@@ -169,7 +194,8 @@ int main(int argc, char *argv[]) {
 	} else {
 		f = fopen(argv[1], "r");
 		if (f == NULL) {
-			printf("grsh: cannot open file\n");
+			char error_message[30] = "An error has occurred\n";
+			write(STDERR_FILENO, error_message, strlen(error_message));
 			return 1;
 		}
 	}
@@ -179,13 +205,11 @@ int main(int argc, char *argv[]) {
 		char *cmd = nextcmd;
 		nextcmd = strtok_r(NULL, "&", &saveptr);
 		while (cmd != NULL) {
-			while (cmd[0] == ' ') {
-				cmd ++;
-			}
-			char *savecmdptr = NULL;
-			char *token = strtok_r(cmd, CMD_DEL, &savecmdptr);
+			trim_whitespace(&cmd);
+			char *rest = NULL;
+			char *token = strtok_r(cmd, WHITESPACE, &rest);
 			if (token != NULL) {
-				process_cmd(&pathnodes, cmd, &cmd[strlen(token)+1], nextcmd != NULL);
+				process_cmd(&pathnodes, cmd, rest, nextcmd != NULL);
 			}
 			cmd = nextcmd;
 			nextcmd = strtok_r(NULL, "&", &saveptr);
