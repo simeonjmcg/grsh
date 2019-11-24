348776f70c9df6d3611d5c10b9356c55f28ff435
Simeon McGraw <simeon@smcgraw.com>
2019-11-24 16:35:34 +0000

    Fixed memory problems

--- src/grsh.c
+++ src/grsh.c
@@ -15,13 +15,16 @@ typedef struct pathnode {
 	struct pathnode *next;
 } pathnode_t;
 
-void deletePath(pathnode_t *n) {
-	while (n != NULL) {
-		pathnode_t *next = n->next;
-		free(n->path);
-		free(n);
-		n = next;
+void delete_path(pathnode_t **head, int *len) {
+	pathnode_t *current = (*head);
+	while (current != NULL) {
+		pathnode_t *next = current->next;
+		free(current->path);
+		free(current);
+		current = next;
 	}
+	(*head) = NULL;
+	(*len) = 0;
 }
 
 void trim_whitespace(char **str) {
@@ -34,36 +37,42 @@ void trim_whitespace(char **str) {
 	}
 }
 
-pathnode_t * addPath(const char *str) {
+pathnode_t * add_path(const char *str, int *len) {
+	int str_len = strlen(str);
+	if (str_len > *len) {
+		(*len) = str_len;
+	}
 	pathnode_t *n = (pathnode_t *) malloc(sizeof(pathnode_t));
-	n->path = (char*) malloc(sizeof(char) * strlen(str));
+	n->path = (char*) malloc(sizeof(char) * str_len + 1);
+	n->next = NULL;
 	strcpy(n->path, str);
 	return n;
 }
 
-char * pathjoin(char *path1, char *path2) {
-	char *dest = (char *)malloc(sizeof(char) * (strlen(path1) + strlen(path2) + 1));
-	strcpy(dest, path1);
-	strcat(dest, "/");
-	strcat(dest, path2);
-	return dest;
+void pathjoin(char *dest, const char *path1, const char *path2, int len) {
+	strncpy(dest, path1, len);
+	len -= strlen(path1);
+	strncat(dest, "/", len);
+	len -= 1;
+	strncat(dest, path2, len);
 }
 
-char * get_full_path(pathnode_t *path, char *cmd) {
+void get_full_path(char *dest, int len, pathnode_t *path, char *cmd) {
 	pathnode_t *node = path;
 	if (access(cmd, X_OK) == 0) {
-		return cmd;
+		strncpy(dest, cmd, len);
+		return;
 	} else {
 		while (node != NULL) {
-			char *full = pathjoin(node->path, cmd);
-			if (access(full, X_OK) == 0) {
-				return full;
+			pathjoin(dest, node->path, cmd, len);
+			if (access(dest, X_OK) == 0) {
+				return;
 			}
-			free (full);
 			node = node->next;
 		}
 	}
-	return NULL;
+	// has not been found, reset first byte so strlen=0
+	dest[0] = 0;
 }
 
 int count_words (char *str) {
@@ -72,56 +81,59 @@ int count_words (char *str) {
 	}
 	int numWords = 0;
 	bool isWord = false;
-	while(*str != 0) {
-		if (isspace(*str) && isWord) {
-			numWords ++;
+	for (int i=0, l=strlen(str); i<l; i++) {
+		if (isspace(str[i]) && isWord) {
 			isWord = false;
-		} else if (!isspace(*str) && !isWord) {
+		} else if (!isspace(str[i]) && !isWord) {
+			numWords ++;
 			isWord = true;
 		}
-		str++;
 	}
 	return numWords;
 }
 
-char ** split_args (char *cmd, char *argstr) {
-	int len = count_words(argstr) + 1;
-	char **args = (char **) malloc(sizeof(char *) * len + 1);
-	
-	char *saveptr = NULL;
-	args[0] = cmd;
-	args[len] = NULL;
+void split_args (char ** dest, int len, char *cmd, char *argstr) {
+	if (len < 2) {
+		return;
+	}
+	dest[0] = cmd;
+	dest[len-1] = NULL;
 	if (argstr == NULL) {
-		return args;
+		return;
 	}
 	int idx = 1;
+	char *saveptr = NULL;
 	char * arg = strtok_r(argstr, WHITESPACE, &saveptr);
-	while (arg != NULL && idx <= len) {
-		args[idx++] = arg;
+	while (arg != NULL && idx < len - 1) {
+		dest[idx++] = arg;
 		arg = strtok_r(NULL, WHITESPACE, &saveptr);
 	}
-	return args;
 }
 
 void execute_cmd (char *full_cmd, char **argv, char *output, bool async) {
 	char *out = strdup(output);
 	int id = fork();
+	if (id < 0) {
+		char error_message[30] = "An error has occurred\n";
+		write(STDERR_FILENO, error_message, strlen(error_message));
+	}
 	if (id == 0) { // child
 		if (out != NULL && strlen(out) > 0) {
-			close(STDOUT_FILENO);
-			close(STDERR_FILENO);
-			open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-			open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+			int id = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+			dup2(id, STDOUT_FILENO);
+			dup2(id, STDERR_FILENO);
 		}
-		execv(full_cmd, argv);
+		int err = execv(full_cmd, argv);
+		exit(err);
+		return;
 	} else {
-		 if (!async) {
-			 waitpid(id, NULL, 0);
-		 }
+		if (!async) {
+			waitpid(id, NULL, 0);
+		}
 	}
 }
 
-void process_cmd (pathnode_t **path, char *cmd, char *argstr, bool async) {
+void process_cmd (pathnode_t **path, int *max_path, char *cmd, char *argstr, bool async) {
 	if (strcmp(cmd, "exit") == 0) {
 		exit(0);
 		return;
@@ -134,13 +146,13 @@ void process_cmd (pathnode_t **path, char *cmd, char *argstr, bool async) {
 			write(STDERR_FILENO, error_message, strlen(error_message));
 		}
 	} else if (strcmp(cmd, "path") == 0) {
-		deletePath(*path);
-		(*path) = NULL;
+		delete_path(path, max_path);
+
 		pathnode_t * curr_node = NULL;
 		char *saveptr = NULL;
 		char *arg = strtok_r(argstr, WHITESPACE, &saveptr);
 		while (arg != NULL) {
-			pathnode_t * node = addPath(arg);
+			pathnode_t * node = add_path(arg, max_path);
 			if (curr_node == NULL) {
 				(*path) = node;
 				curr_node = (*path);
@@ -151,9 +163,11 @@ void process_cmd (pathnode_t **path, char *cmd, char *argstr, bool async) {
 			arg = strtok_r(NULL, WHITESPACE, &saveptr);
 		}
 	} else if(strlen(cmd) > 0) {
-		char *full_path = get_full_path(*path, cmd);
-		
-		if (full_path == NULL) {
+		int full_path_len = (*max_path) + 1 + strlen(cmd) + 1;
+		char *full_path = (char *) malloc (sizeof (char) * full_path_len);
+		get_full_path(full_path, full_path_len, *path, cmd);
+
+		if (strlen(full_path) == 0) {
 			char error_message[30] = "An error has occurred\n";
 			write(STDERR_FILENO, error_message, strlen(error_message));
 			return;
@@ -175,9 +189,12 @@ void process_cmd (pathnode_t **path, char *cmd, char *argstr, bool async) {
 			trim_whitespace(&output_file);
 		}
 
-		char **argv = split_args(cmd, args);
+		int len = count_words(args) + 2;
+		char **argv = (char **) malloc(sizeof(char *) * len);
+		split_args(argv, len, cmd, args);
 		execute_cmd(full_path, argv, output_file, async);
 		free(argv);
+		free(full_path);
 	}
 }
 
@@ -187,7 +204,8 @@ int main(int argc, char *argv[]) {
 		write(STDERR_FILENO, error_message, strlen(error_message));
 		return 1;
 	}
-	pathnode_t *pathnodes = addPath("/bin");
+	int max_path = 0;
+	pathnode_t *pathnodes = add_path("/bin", &max_path);
 
 	char *buff = NULL;
 	size_t size;
@@ -214,7 +232,7 @@ int main(int argc, char *argv[]) {
 			char *rest = NULL;
 			char *token = strtok_r(cmd, WHITESPACE, &rest);
 			if (token != NULL) {
-				process_cmd(&pathnodes, cmd, rest, nextcmd != NULL);
+				process_cmd(&pathnodes, &max_path, cmd, rest, nextcmd != NULL);
 			}
 			cmd = nextcmd;
 			nextcmd = strtok_r(NULL, "&", &saveptr);
